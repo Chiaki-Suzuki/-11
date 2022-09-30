b50c80ab146a357f8e70f4686395a31183afc977
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-30 17:00:39 +0900

    リファクタリング

--- js/main.js
+++ js/main.js
@@ -1,113 +1,76 @@
 let app = new Vue({
   el: '#app',
   data: {
+    // コース表示
     course1: true,
     course2: true,
     course3: true,
     course4: true,
-    course1Hair: 'noselect',
+    // 料金計算
     course1weight: 'noselect',
     course1Option: false,
-    course2Hair: 'noselect',
     course2weight: 'noselect',
     course2Option: false,
     course3menu: 'noselect',
     course4Option: false,
     price: 0
   },
+  filters: {
+    number_format: function (num) {
+      return num.toLocaleString()
+    }
+  },
   methods: {
-    // コース１
-    test: function () {
+    /*-------------------------
+      コース料金
+    -------------------------*/
+    courseCalc: function (val, price, add, add2) {
       let addPrice = 0
 
-      // 3kg以下
-      if (this.course1weight == 0) {
-        addPrice = 13000
+      // 3kg以下（シャンプー・カットなし）
+      if (val == 0) {
+        addPrice = price
       }
-      // 4kg～7kg
-      if (this.course1weight == 1) {
-        addPrice = 17000
+      // 4kg～7kg（シャンプー無し・カットあり）
+      if (val == 1) {
+        addPrice = price + add
       }
-      // 7kg以上
-      if (this.course1weight == 2) {
-        addPrice = 20000
+      // 7kg以上（シニアわんトリミング シャンプー＆カット）
+      if (val == 2) {
+        addPrice = price + add2
       }
       return addPrice;
     },
-    // コース２
-    test2: function() {
-      let addPrice = 0
-
-      // 3kg以下
-      if (this.course2weight == 0) {
-        addPrice = 25000
-      }
-      // 4kg～7kg
-      else if (this.course2weight == 1) {
-        addPrice = 30000
-      }
-      // 7kg以上
-      else if (this.course2weight == 2) {
-        addPrice = 35000
-      }
-      return addPrice;
-    },
-    // コース３
-    test3: function() {
-      let addPrice = 0
-
-      // シャンプー・カットなし
-      if (this.course3menu == 0) {
-        addPrice = 10000
-      }
-      // シャンプー無し・カットあり
-      else if (this.course3menu == 1) {
-        addPrice = 20000
-      }
-      // シニアわんトリミング シャンプー＆カット
-      else if (this.course3menu == 2) {
-        addPrice = 15000
-      }
-      return addPrice;
-    },
-    // コース１オプション
-    test1option: function () {
+    /*-------------------------
+      オプション料金
+    -------------------------*/
+    optionCalc: function (val, price) {
       let addOption = 0
-      if (this.course1Option) {
-        addOption = 3000
-      } else {
+
+      // オプションあり
+      if (val) {
+        addOption = price
+      }
+      // オプションなし
+      else {
         addOption = 0
       }
       return addOption;
     },
-    // コース２オプション
-    test2option: function () {
-      let addOption = 0
-      if (this.course2Option) {
-        addOption = 3000
-      } else {
-        addOption = 0
-      }
-      return addOption;
-    },
-    // コース４オプション
-    test4option: function () {
-      let addOption = 0
-      if (this.course4Option) {
-        addOption = 10000
-      } else {
-        addOption = 0
-      }
-      return addOption;
-    },
+    /*-------------------------
+      合計金額
+    -------------------------*/
     totalPrice: function () {
-      let course1Price = this.test()
-      let course2Price = this.test2()
-      let course3Price = this.test3()
-      let optionPrice = this.test1option()
-      let option2Price = this.test2option()
-      let option4Price = this.test4option()
-      this.price = course1Price + course2Price + course3Price + optionPrice + option2Price + option4Price
+      // コース料金
+      let course1Price = this.courseCalc(this.course1weight, 13000, 4000, 7000)
+      let course2Price = this.courseCalc(this.course2weight, 25000, 5000 , 10000)
+      let course3Price = this.courseCalc(this.course3menu, 10000, 10000, 5000)
+      // オプション料金
+      let option1Price = this.optionCalc(this.course1Option, 3000)
+      let option2Price = this.optionCalc(this.course2Option, 3000)
+      let option4Price = this.optionCalc(this.course4Option, 10000)
+      // 合計
+      this.price = course1Price + course2Price + course3Price + option1Price + option2Price + option4Price
     }
   }
 })
